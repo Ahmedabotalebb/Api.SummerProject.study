0dcfd9ec661d165a30dea0eee1659e03309b7750
Ahmed abotaleb <ahmedabotaleb123555@gmail.com>
2025-07-19 20:06:31 +0300

    Create Product Dtos And Product Service

---
+++ Core/ServiceAbstrastion/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.DataTransfereObjects;
+
+namespace ServiceAbstrastion
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+
+        Task<IEnumerable<BrandDto>> GetAllBrandsAsync();
+        Task<IEnumerable<TypesDto>> GetAllTypesAsync();
+        Task<ProductDto> GetProductByIdAsync(int id);
+    }
+}
