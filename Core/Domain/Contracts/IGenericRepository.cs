5d89b88e9a0b19ce2c7374706090eb569c3fa513
Ahmed abotaleb <ahmedabotaleb123555@gmail.com>
2025-07-19 06:12:50 +0300

    Generic Repository

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity,Tkey> where TEntity : BaseEntity<Tkey>
+    {
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+        public Task<TEntity?> GetByIdAsync(Tkey id);
+        public Task AddAsync(TEntity entity);
+        public void Update(TEntity entity);
+        public void Delete(TEntity entity);
+        
+
+    }
+}
