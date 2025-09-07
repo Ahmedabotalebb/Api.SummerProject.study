0e5307e38f30a3519064a0f9220bb815bb366f89
Ahmed abotaleb <ahmedabotaleb123555@gmail.com>
2025-09-08 00:55:55 +0300

    Order Module

---
+++ Core/Domain/Models/OrderModule/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.OrderModule
+{
+    public class Order:BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; } = default!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        OrderStatus Status { get; set; }
+
+        DeliveryMethod DeliveryMethod { get; set; } = default!;
+        public int DeliveryMethodId { get; set; } //fk
+
+
+        ShipingAddress ShipingAddress { get; set; } = default!;
+        public ICollection<OrderItem> Items { get; set; } = [];
+        public decimal SubTotal { get; set; }
+
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Price;
+
+    }
+}
---
+++ Core/Domain/Models/OrderModule/ProductItemOredered.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.OrderModule
+{
+    public class ProductItemOredered
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
+    }
+}
